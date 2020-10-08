7722ba1f9014680844461041f8e2d732d6e10f0a
velinovasen <velinovasen@users.noreply.github.com>
2020-10-08 23:55:50 +0300

    'volume_scr'

--- games/models.py
+++ games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BetsVolume(models.Model):
+    day = models.CharField()
+    time = models.TimeField()
+    home_team = models.CharField(max_length=60)
+    away_team = models.CharField(max_length=60)
+    final_bet = models.CharField(max_length=1)
+    odds = models.FloatField()
+    amount = models.FloatField()
+
+    def __str__(self):
+        return f'{self.home_team} - {self.away_team} - ' \
+               f'{self.final_bet} - {self.odds} - {self.amount}'
---
+++ scrapers/best_volume.py
@@ -0,0 +1,99 @@
+from selenium.webdriver import Chrome, ChromeOptions
+import bs4
+import re
+from time import sleep
+from games.models import BetsVolume
+
+
+class Volume:
+
+    WEB_LINKS = {
+        "football": "https://www.bahisanaliz14.com/avrupa-en-cok-oynanan-maclar/"
+    }
+
+    def scrape(self):
+
+        # # CONNECTING THE DATABASE
+        # connector = sqlite3.connect('games-db')
+        # cursor = connector.cursor()
+        # cursor.execute('DROP TABLE IF EXISTS VolumeGames')
+        # cursor.execute('CREATE TABLE VolumeGames(day TEXT, time TEXT, home_team TEXT,'
+        #                ' away_team TEXT, bet_sign INTEGER, odd REAL, volume REAL)')
+
+        days_numbered = {
+            "Pts": "Monday", "Sal": "Tuesday", "Çar": "Wednesday", "Per": "Thursday",
+            "Cum": "Friday", "Cts": "Saturday", "Pzr": "Sunday"
+        }
+
+        # OPEN THE WEBSITE AND GET THE DATA
+        options = ChromeOptions()
+        options.headless = True
+        options.add_argument("--lang=en")
+        driver = Chrome(options=options, executable_path='C://Windows/chromedriver.exe')
+        driver.get(self.WEB_LINKS["football"])
+        sleep(2)
+        html = driver.execute_script("return document.documentElement.outerHTML;")
+        driver.close()
+
+        # WORK WITH THE DATA AND GO THROUGH VOLUMES
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        matches = soup.find_all(class_=re.compile('IH2Satir'))
+
+        BetsVolume.objects.all().delete()
+
+        the_bulk = []
+
+        for game in matches:
+            elements = list(game)
+            #print(elements)
+
+            # GET THE TIME
+            time_tokens = str(elements[1])
+            time_pattern = r'[ ](\d+[:]\d+)\<\/'
+            day_pattern = r'\;\"\>(.{3})\,'
+            time_raw = re.search(time_pattern, time_tokens)
+            day_raw = re.search(day_pattern, time_tokens)
+            time = time_raw.group(1)
+            day = days_numbered[day_raw.group(1)]
+            #print(days_numbered[day])
+
+            # GET THE TEAMS
+            team_tokens = str(elements[2])
+            teams_pattern = r'[o][n][g]\>(.+)\<\/[s][t][r]'
+            teams = re.search(teams_pattern, team_tokens)
+            home_team, away_team = teams.group(1).split(' - ')
+
+            # GET THE BET POSITION
+            position_token = str(elements[3])
+            position_pattern = r'[s][p][a][n]\>[ ](\d{1})\<\/[d][i]'
+            position = re.search(position_pattern, position_token)
+            final_bet = position.group(1)
+
+            # GET THE ODD
+            odds_token = str(elements[4])
+            odds_pattern = r'[a][n]\>(\d+\.\d+)\<\/'
+            odds_raw = re.search(odds_pattern, odds_token)
+            odds = odds_raw.group(1)
+
+            # GET THE AMOUNT
+            amount_token = str(elements[5])
+            amount_pattern = r'[o][n][g]\>(.+)[ ][A-z]+'
+            amount = re.search(amount_pattern, amount_token)
+            total_amount = amount.group(1)
+
+            the_bulk.append(BetsVolume(day=day, time=time, home_team=home_team,
+                                       away_team=away_team, final_bet=final_bet,
+                                       odds=odds, amount=amount))
+
+        BetsVolume.objects.bulk_create()
+
+        #     cursor.execute('INSERT INTO VolumeGames(day, time, home_team, away_team, bet_sign,'
+        #                    ' odd, volume) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        #                    (day, time, home_team, away_team, final_bet, odds, total_amount))
+        #     connector.commit()
+        #
+        # connector.close()
+
+
+vlm = Volume()
+vlm.scrape()
